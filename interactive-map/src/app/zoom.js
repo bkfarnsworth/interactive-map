ab051f9e418de0c5f43681bd3980ed7638cf2414
Miles Farnsworth <mfarns135@gmail.com>
2023-07-13 11:20:29 -0700

    working on zoom
    making the zoom function work correctly, not quite there

--- interactive-map/src/app/zoom.js
+++ interactive-map/src/app/zoom.js
@@ -3,20 +3,23 @@ let height = 2901;
 let width = 4498;
 const MAX = 3;
 const MIN = .325;
-function zoom(event){  
-    event.preventDefault(); 
-    console.log(event.deltaY)
-    if (event.deltaY < 0 && standard < MAX){
-     standard += .1
+function zoom(event) {
+    event.preventDefault();
+    if (event.deltaY < 0 && standard < MAX) {
+        standard += .1
     }
     if (event.deltaY > 0 && standard > MIN) {
-     standard -= .1
+        standard -= .1
     }
+    // console.log('offsetX' + event.offsetX)
+    let offsetX = event.offsetX;
+    let offsetY = event.offsetY;
     let level = Math.min(Math.max(standard, MIN), MAX)
     let newHeight = height * level;
     let newWidth = width * level;
     document.querySelector('#bigMap').height = newHeight;
     document.querySelector('#bigMap').width = newWidth;
-    console.log(`newHeight:${newHeight},newWidth:${newWidth},Standard:${standard}`)
+    // document.querySelector('#mapContainer').scrollTo(event.offsetX, event.offsetY)
+    document.querySelector('#mapContainer').scrollBy(event.offsetX * level , event.offsetY * level)
 }
 export default zoom
